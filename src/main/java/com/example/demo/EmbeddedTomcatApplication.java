edf4a82f9f1ab58c8713081810dcb66ae5235a70
Josue <josue.morales35@outlook.com>
2024-05-13 13:09:44 -0600

    Hola MUNDO added for testing

--- src/main/java/com/example/demo/EmbeddedTomcatApplication.java
+++ src/main/java/com/example/demo/EmbeddedTomcatApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmbeddedTomcatApplication {
 
     public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!");
         SpringApplication.run(EmbeddedTomcatApplication.class, args);
     }
 }
